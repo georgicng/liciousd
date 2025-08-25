e2611bcd7ae78ca38f9323f168d09836092c3e36
Ikpugbu George <georgicng.2.0@gmail.com>
2025-08-25 18:46:29 +0100

    fix: Use vue component for faq

--- Theme/src/Resources/views/components/shortcodes/faqs/index.blade.php
+++ Theme/src/Resources/views/components/shortcodes/faqs/index.blade.php
@@ -3,3 +3,45 @@
         {!! $slot !!}
     </div>
 </div>
+
+@pushOnce('scripts')
+    <script type="text/x-template" id="v-accordion-template">
+        <div class="cr-accordion-item overflow-hidden mb-[10px] border-[1px] border-solid border-[#eee] rounded-[5px]">
+            <h4
+                class="accordion-head active-arrow m-[0] p-[14px] text-[#4b5966] text-[16px] leading-[20px] font-medium relative border-b-[1px] border-solid border-[#eee] font-Poppins cursor-pointer tracking-[0] max-[767px]:text-[15px]"
+                :class="isOpen ? 'active-arrow' : ''"
+                @click="toggle">
+                @{{ question }}
+            </h4>
+            <div v-show="isOpen" class="accordion-body py-[15px] p-[15px] text-[14px] font-Poppins text-[#7a7a7a] leading-[1.75]">
+                <div v-html="answer"></div>
+            </div>
+        </div>
+    </script>
+
+    <script type="module">
+        app.component('v-accordion', {
+            template: '#v-accordion-template',
+
+            props: [
+                'isActive',
+                'question',
+                'answer',
+            ],
+
+            data() {
+                return {
+                    isOpen: this.isActive,
+                };
+            },
+
+            methods: {
+                toggle() {
+                    this.isOpen = ! this.isOpen;
+
+                    this.$emit('toggle', { isActive: this.isOpen });
+                },
+            },
+        });
+    </script>
+@endPushOnce
--- Theme/src/Resources/views/components/shortcodes/faqs/item.blade.php
+++ Theme/src/Resources/views/components/shortcodes/faqs/item.blade.php
@@ -1,10 +1,3 @@
 @props(['question' => 'Instagram Feed', 'answer' => 'Instagram Feed'])
 
-<div class="cr-accordion-item border-[#eee] overflow-hidden mb-[10px] border-[1px] border-solid border-[#eee] rounded-[5px]">
-    <h4 class="accordion-head active-arrow m-[0] p-[14px] text-[#4b5966] text-[16px] leading-[20px] font-medium relative border-b-[1px] border-solid border-[#eee] font-Poppins cursor-pointer tracking-[0] max-[767px]:text-[15px]">
-        {{ $question }}
-    </h4>
-    <div class="accordion-body py-[15px] p-[15px]">
-        <p class="text-[14px] font-Poppins text-[#7a7a7a] leading-[1.75]">{!! $answer !!}</p>
-    </div>
-</div>
+<v-accordion question="{{ $question }}" answer="{{ $answer }}"></v-accordion>
